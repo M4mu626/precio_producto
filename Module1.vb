18b5ec40411f2ddf8e2449c93b3fceeeccaa7630
M4mu626 <115762471+M4mu626@users.noreply.github.com>
2022-12-04 12:14:22 -0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Cantidad, Credito As Integer
+        Dim precio, abono, cuota1, cuota2 As Single
+        Console.WriteLine("Que cantidad desea llevar: ")
+        Cantidad = Console.ReadLine()
+        Console.WriteLine("Que precio tiene el producto: ")
+        precio = Console.ReadLine()
+        abono = Cantidad * precio
+        Console.WriteLine("El precio total a pagar es: ")
+        Console.WriteLine(abono)
+        Console.WriteLine("cuanto desea abonar ")
+        Credito = Console.ReadLine()
+        cuota1 = abono - Credito
+        If Credito < abono Then
+            Console.WriteLine("El total que queda debiendo es:")
+            Console.WriteLine(cuota1)
+        Else
+            cuota2 = abono - Credito
+            Console.WriteLine("El total que queda debiendo es:")
+            Console.WriteLine(cuota2)
+
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
